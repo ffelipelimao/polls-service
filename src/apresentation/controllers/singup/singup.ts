bd93433ccef93345b0f9170a00ec01a4950a0918
Luiz Felipe Limão <lflimao@hotmail.com>
2021-02-20 13:16:17 -0300

    refactor: move http response ok to a helper method

--- src/apresentation/controllers/singup/singup.ts
+++ src/apresentation/controllers/singup/singup.ts
@@ -1,6 +1,6 @@
 import { HttpResponse, HttpRequest, Controller, EmailValidator, AddAccount } from './singup-protocols'
 import { MissingParamError, InvalidParamError } from '../../erros'
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
 
 export class SingUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -32,10 +32,7 @@ export class SingUpController implements Controller {
         email,
         password
       })
-      return {
-        statusCode: 200,
-        body: account
-      }
+      return ok(account)
     } catch (error) {
       return serverError()
     }
