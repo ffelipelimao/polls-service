7460ef5257f6c381f9ae8f85750c3509fe3023c3
Luiz Felipe Limão <luiz.limao@midway.com.br>
2021-04-05 17:55:58 -0300

    refactor: move map to mongo helper

--- src/infra/database/mongodb/helpers/mongo-helper.ts
+++ src/infra/database/mongodb/helpers/mongo-helper.ts
@@ -14,5 +14,11 @@ export const MongoHelper = {
   },
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectionWihtoutId } = collection
+    return { id: _id, ...collectionWihtoutId }
   }
+
 }
